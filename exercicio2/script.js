ff1774d51d65669bad627d67aa6df85b84ee0d6e
jBrito94 <jbritodev@gmail.com>
2026-04-10 20:28:24 +0100

    exercicio 3

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -40,3 +40,22 @@ localStorage.setItem("user", JSON.stringify(user));
 
 let u = JSON.parse(localStorage.getItem("user"));
 console.log(u);
+
+// EXAMPLE 4
+
+let theme = localStorage.getItem("theme");
+let themeButton = document.getElementById("theme-button");
+
+theme ? (themeButton.innerText = theme) : (themeButton.innerText = "light");
+
+themeButton.addEventListener("click", () => {
+  if (theme === "light") {
+    localStorage.setItem("theme", "dark");
+    theme = "dark";
+    themeButton.innerText = "dark";
+  } else {
+    localStorage.setItem("theme", "light");
+    theme = "light";
+    themeButton.innerText = "light";
+  }
+});
